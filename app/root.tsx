ae349f66d4bd463cb370d0289a32b36ab1f01da0
kouji_kawamura <45913424+kouji0705@users.noreply.github.com>
2025-04-29 11:19:00 +0900

    add:

--- app/root.tsx
+++ app/root.tsx
@@ -24,7 +24,7 @@ export const links: LinksFunction = () => [
 
 export default function App() {
 	return (
-		<html lang="en" suppressHydrationWarning>
+		<html lang="ja" suppressHydrationWarning>
 			<head>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
